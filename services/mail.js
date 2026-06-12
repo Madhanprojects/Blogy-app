bef0c7e6dd246af2c4b379f24b672e88aa69da80
Madhan Bandi <madhanbandi74@gmail.com>
2026-06-12 12:14:54 +0530

    updated

--- services/mail.js
+++ services/mail.js
@@ -4,17 +4,22 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: "userv9796@gmail.com",
-        pass: process.env.pass
+        pass: `${process.env.pass}`
     }
 });
 
 async function sendOTP(email, otp){
+    try {
     await transporter.sendMail({
         from: "userv9796@gmail.com",
         to: email,
         subject: "Blogy Email Verification",
         text: `Your OTP is ${otp}`
     });
+} catch (err) {
+    console.error("MAIL ERROR:", err);
+    throw err;
+}
 
 }
 
